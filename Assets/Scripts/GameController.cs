d81dbf5e5f1dfe5a45f6f0e0879579f8e2445b2f
Oskar van Velden <oskar@rakso.nl>
2019-02-19 08:27:43 +0100

    Add game to git

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Button higherButton;
+    public Button lowerButton;
+    public Text guessedNumberTextfield;
+
+    public int minimum;
+    public int maximum;
+
+    private int minGuess;
+    private int maxGuess;
+
+    int guessedNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        higherButton.onClick.AddListener(higherHandler);
+        lowerButton.onClick.AddListener(lowerHandler);
+
+        minGuess = minimum;
+        maxGuess = maximum;
+
+        updateTextfield();
+    }
+
+    private void higherHandler() {
+        minGuess = guessedNumber + 1;
+
+        updateTextfield();
+    }
+
+    private void lowerHandler() {
+        maxGuess = guessedNumber - 1;
+
+        updateTextfield();
+    }
+
+    private void updateTextfield() {
+        guessedNumber = UnityEngine.Random.Range(maxGuess, minGuess);
+        guessedNumberTextfield.text = guessedNumber.ToString();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
